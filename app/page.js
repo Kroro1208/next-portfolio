5806a2e62a06f03359a6f41f7becc0a42178119a
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-01-07 19:13:58 +0900

    各page component作成, main css設定

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+import Hero from '@/components/Hero'
+import Join from '@/components/Join'
+import Plans from '@/components/Plans'
+import Programs from '@/components/Programs'
+import Reasons from '@/components/Reasons'
+import Testimonials from '@/components/Testimonials'
+import Footer from '@/components/Footer'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      <p className='bg-sky'>テスト</p>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </main>
   )
 }
