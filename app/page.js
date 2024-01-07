8d3949ea3ea344cfed5896be7ec1a22383b6e309
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-01-07 18:52:36 +0900

    color設定

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
+      <p className='bg-sky'>テスト</p>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        lightGray: '#d3d3d3',
+        gray: '#778899',
+        darkGray: '#464d53',
+        darkdarkGray: '#363d42',
+        pink: '#fa8072',
+        green: '#13ce66',
+        blue: '#1fb6ff',
+        orange: '#ff7849',
+        cyan: '#a5f3fc',
+        amber: '#fde68a',
+        emerald: '#a7f3d0',
+        sky: '#7dd3fc',
+        indigo: '#a5b4fc',
+        rose: '#fecdd3'
+      },
     },
   },
   plugins: [],
-}
+};
